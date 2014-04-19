d74fcc2498b0f479749cdc5529981b37447ba81e
Ilion Beyst <ilion.beyst@gmail.com>
2014-04-19 14:18:36 +0200

    added vimrc

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+REPO=https://github.com/TovenaarKlus/dotfiles.git
+DEST="$HOME/.dotfiles"
+
+if [[ -d $DEST ]]; then
+    cd $DEST && git pull origin master && cd -
+else
+    echo "cloning repo: $REPO into dir: $DEST"
+    git clone $REPO $DEST
+fi
+
+function setup(){
+    SRC="$1"
+    DST="$2"
+    echo "Installing $SRC"
+    mkdir -p $(dirname "$DST")
+    ln -sfn "$DEST/$SRC" "$DST"
+}
+
+setup vimrc "$HOME/.vimrc"
+
+if [[ -d $HOME/.vim/bundle/neobundle.vim ]]; then
+    echo "NeoBundle is installed"
+else
+    echo "Setting up Neobundle"
+    curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
